d023ad373378843c9d9b7a8e05ff3341a54d03b9
limpid8818 <2388874604@qq.com>
2025-08-01 11:24:23 +0800

    Weather Service Pusher: Except Timeout

--- service/weather/client.py
+++ service/weather/client.py
@@ -1,5 +1,7 @@
 import httpx
 from typing import Optional
+
+from infra.logger import logger
 from .models import Location, NowWeather, DailyForecast
 from infra.config.settings import settings
 
@@ -11,12 +13,18 @@ class QWeatherClient:
         self.api_key = settings.WEATHER_API_KEY
         self.client = httpx.AsyncClient(
             headers={"X-QW-Api-Key": self.api_key},
+            timeout=httpx.Timeout(10, connect=5),
         )
 
     async def get_location(self, city: str) -> Optional[Location]:
         url = f"https://{self.api_host}/geo/v2/city/lookup"
         params = {"location": city}
-        resp = await self.client.get(url, params=params)
+
+        try:
+            resp = await self.client.get(url, params=params)
+        except httpx.Timeout:
+            logger.warn("Weather", f"[{city}] Get Loc Timeout")
+            return None
 
         if resp.status_code != 200:
             print(f"[ERROR] HTTP {resp.status_code}, body: {resp.text}")
@@ -42,7 +50,11 @@ class QWeatherClient:
     async def get_now_weather(self, location_id: str) -> Optional[NowWeather]:
         url = f"https://{self.api_host}/v7/weather/now"
         params = {"location": location_id}
-        resp = await self.client.get(url, params=params)
+        try:
+            resp = await self.client.get(url, params=params)
+        except httpx.Timeout:
+            logger.warn("Weather", f"[{location_id}] Get Now Weather Timeout")
+            return None
         data = resp.json()
         if data.get("code") == "200":
             now = data["now"]
@@ -52,7 +64,11 @@ class QWeatherClient:
     async def get_daily_forecast(self, location_id: str) -> Optional[list[DailyForecast]]:
         url = f"https://{self.api_host}/v7/weather/7d"
         params = {"location": location_id}
-        resp = await self.client.get(url, params=params)
+        try:
+            resp = await self.client.get(url, params=params)
+        except httpx.Timeout:
+            logger.warn("Weather", f"[{location_id}] Get Forecast Weather Timeout")
+            return None
         data = resp.json()
         if data.get("code") == "200":
             return [DailyForecast(**item) for item in data["daily"]]
